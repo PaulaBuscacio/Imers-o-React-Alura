014bcc0951b76c2ae8dfafb47d5603ae3ea7101a
PaulaBuscacio <paula@buscacio.net>
2020-07-30 18:21:03 -0300

    30/7

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import { Link } from 'react-router-dom';
-import './index.css'
-import Home from './pages/Home'
+import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import CadastroVideo from './pages/cadastro/Video'
-import CadastroCategoria from './pages/cadastro/Categoria'
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 // const Pagina404 = () => (<div>Página não encontrada</div>)
 
-const Pagina404 = () => (<a href="https://mariosouto.com/flappy-bird-devsoutinho/"><h1>😀Clique aqui e tenha uma surpresa👍</h1></a>)
+const Pagina404 = () => (<a href="https://mariosouto.com/flappy-bird-devsoutinho/"><h1>😀Clique aqui e tenha uma surpresa👍</h1></a>);
 ReactDOM.render(
-	<BrowserRouter>
-		<Switch>
-			<Route path="/" component={Home} exact />
-			<Route path="/cadastro/video" component={CadastroVideo} />
-			<Route path="/cadastro/categoria" component={CadastroCategoria} />
-			<Route component={Pagina404} />
-		</Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
